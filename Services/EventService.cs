adf7538ac2b6908de9539eec7c3585d7af5c7eac
Chahalgr <gurleensingh94647@gmail.com>
2025-07-14 18:44:24 -0500

    Added Input Validation

--- Services/EventService.cs
+++ Services/EventService.cs
@@ -7,11 +7,35 @@ public class EventService
 
     public void add(string name, string description, string date, string img)
     {
+        if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(date) ||
+            !DateTime.TryParseExact(date, "MM/dd/yyyy", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out _) 
+            || string.IsNullOrWhiteSpace(description))
+        {
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(img))
+        {
+            img = "https://th.bing.com/th/id/R.c62ac83ee279aaeb77464c545e1d330a?rik=fmsdkS6UFpex0g&riu=http%3a%2f%2fclipartmag.com%2fimages%2foops-emoticon-4.jpg&ehk=VigMrniR44COuiN2v1g4rmABx74eE%2bIVRD0dmDUajLw%3d&risl=&pid=ImgRaw&r=0";
+        }
+
         Events.Add(new Event(Events.Count + 1, name, description, date, img));
     }
 
     public void add(Event e)
     {
+        if (e == null || string.IsNullOrWhiteSpace(e.Name) || string.IsNullOrWhiteSpace(e.Date) 
+            || !DateTime.TryParseExact(e.Date, "MM/dd/yyyy", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out _)
+            || string.IsNullOrWhiteSpace(e.Description))
+        {
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(e.Image))
+        {
+            e.Image = "https://th.bing.com/th/id/R.c62ac83ee279aaeb77464c545e1d330a?rik=fmsdkS6UFpex0g&riu=http%3a%2f%2fclipartmag.com%2fimages%2foops-emoticon-4.jpg&ehk=VigMrniR44COuiN2v1g4rmABx74eE%2bIVRD0dmDUajLw%3d&risl=&pid=ImgRaw&r=0";
+        }
+            
         if(e.Id < 0)
         {
             e.Id = Events.Count + 1;
