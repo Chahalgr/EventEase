17f19eadf4c39f02367fca7a83669e31a68197b6
Chahalgr <gurleensingh94647@gmail.com>
2025-07-14 21:58:25 -0500

    app done!

--- Services/EventService.cs
+++ Services/EventService.cs
@@ -2,7 +2,9 @@ using EventEase.Models;
 public class EventService
 {
     public List<Event> Events { get; set; } = new List<Event>{
-        new Event(1, "Cricket World Cup 2025", "8 teams, 1 Champion!", "01/12/2025", "https://tse1.mm.bing.net/th/id/OIP.RvpXFtpgkyQhqAETu5rD5wHaEY?r=0&rs=1&pid=ImgDetMain&o=7&rm=3")
+        new Event(1, "Cricket World Cup 2025", "8 teams, 1 Champion!", "01/12/2025", "https://tse1.mm.bing.net/th/id/OIP.RvpXFtpgkyQhqAETu5rD5wHaEY?r=0&rs=1&pid=ImgDetMain&o=7&rm=3"),
+        new Event(2, "WWDC2025", "Innovations in technology", "02/15/2025", "https://images.cgames.de/images/gamestar/4/apple-event-teaser_6194070.jpg"),
+        new Event(3, "Eminem Concert 2025", "If you had one shot, would you capture it? Or would you let it slip?", "03/20/2025", "https://tse1.mm.bing.net/th/id/OIP.CXSgkl1mLAFky5heyk7_wQHaEK?r=0&rs=1&pid=ImgDetMain&o=7&rm=3")
     };
 
     public void add(string name, string description, string date, string img)
